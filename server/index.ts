5886845f1310e98e5059b9fbfed8ccc944ab1d2a
pltdeveloper393 <cheltimet@gmail.com>
2026-02-27 14:04:16 +0300

    feat: добавлен файл точки входа бэка сервера (express, middleware, routs, public)

---
+++ server/index.ts
@@ -0,0 +1,39 @@
+import express from 'express';
+import session from 'express-session';
+import path from 'path';
+import authRoutes from './routes/auth';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false, maxAge: 24 * 60 * 60 * 1000 }
+}));
+
+// ========== БУДУЩИЕ РОУТЫ ==========
+// (Названия первичные, могут отличаться и изменяться)
+app.use('/api/auth', authRoutes);
+
+// import catalogRoutes from './routes/catalog';
+// import cartRoutes from './routes/cart';
+// import deliveryRoutes from './routes/delivery';
+//
+// app.use('/api/catalog', catalogRoutes);  // для products
+// app.use('/api/cart', cartRoutes);        // для cart
+// app.use('/api/delivery', deliveryRoutes);// для delivery
+
+app.use(express.static(path.join(__dirname, '../../public')));
+
+// SPA в каркас index.html помещаем
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
