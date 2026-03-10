d56e693115e5c33af3f8afa1c3cf98df6773534e
rxxksyan <n.smantser555@gmail.com>
2026-03-10 10:42:11 +0300

    feat: определение API роутов корзины
    REST endpoints:
    - GET /, GET /count, POST /, PUT /, DELETE /:productId, DELETE /

---
+++ server/routes/route_cart.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import { 
+  getCart, 
+  addItemToCart, 
+  updateItemQuantity, 
+  removeItemFromCart, 
+  clearUserCart,
+  getCartItemCount 
+} from '../controllers/controller_cart';
+
+const router = Router();
+
+router.get('/', getCart);
+
+router.get('/count', getCartItemCount);
+
+router.post('/', addItemToCart);
+
+router.put('/', updateItemQuantity);
+
+router.delete('/:productId', removeItemFromCart);
+
+router.delete('/', clearUserCart);
+
+export default router;
