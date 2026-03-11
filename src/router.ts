d4ff7a2ae802a2899f95fcd63d3aeb1957353c73
Idhenxysvs <idhhgj0@gmail.com>
2026-03-11 12:32:41 +0300

    feat: Начальная настройка проекта - создана папка public с файлами index.html и styles.css; - создана папка src с файлами index.ts и public.ts

---
+++ src/router.ts
@@ -0,0 +1,39 @@
+type PageFunction = () => void | Promise<void>;
+
+export class Router {
+  private routes: Map<string, PageFunction> = new Map();
+  
+  constructor() {
+    window.addEventListener('popstate', () => this.handleRoute());
+  }
+  
+  addRoute(path: string, handler: PageFunction) {
+    this.routes.set(path, handler);
+  }
+  
+  navigateTo(path: string) {
+    window.history.pushState({}, '', path);
+    this.handleRoute();
+  }
+  
+  private async handleRoute() {
+    const path = window.location.pathname;
+    const handler = this.routes.get(path) || this.routes.get('/');
+    if (handler) {
+      await handler();
+    }
+  }
+  
+  start() {
+    this.handleRoute();
+  }
+}
+
+export const router = new Router();
+
+// ========================================================
+import { renderDeliveryPage } from './pages/DeliveryPage_delivery.js';
+
+router.addRoute('/', renderDeliveryPage);
+router.addRoute('/delivery', renderDeliveryPage);
+// ========================================================
